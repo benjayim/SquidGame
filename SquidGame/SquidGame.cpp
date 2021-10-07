e7cf899262385edeb4e23e3140725281cf7df61e
Benjamin Yim <benjayim@microsoft.com>
2021-10-07 10:39:59 -0700

    implement

--- SquidGame/SquidGame.cpp
+++ SquidGame/SquidGame.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+using namespace std;
+
+double CalculateProbability(unsigned int numSteps, unsigned int numPlayers)
+{
+    // If there are no steps left, you win!
+    if (numSteps == 0)
+    {
+        return 1;
+    }
+
+    // If everyone's dead, you lose!
+    if (numPlayers == 0)
+    {
+        return 0;
+    }
+
+    // You choose wrong -> One less player (cuz you dead), one less step (since the next player can deduce the right step)
+    // You choose right -> One less step, same number of players
+    return ((0.5 * CalculateProbability(numSteps - 1, numPlayers - 1)) + (0.5 * CalculateProbability(numSteps - 1, numPlayers)));
+}
+
 int main()
 {
+    const unsigned int numSteps = 18;
+    
+    for (unsigned int playerNum = 0; playerNum <= 16; playerNum++)
+    {
+        cout << playerNum << ": " << CalculateProbability(numSteps, playerNum) << endl;
+    }
+
     return 0;
 }
